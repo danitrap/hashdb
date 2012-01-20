f58390013f71f717ba0028ca59e8f6890157558e
Daniele Trapani <daniele.trapani@live.it>
2012-01-20 05:02:41 +0100

    added comments

--- hashdb.rb
+++ hashdb.rb
@@ -5,12 +5,14 @@ require 'json'
 require 'net/http'
 
 def hash_search( query )
+  #gets the json output from goog.li servers
   url = "http://goog.li/?j=#{URI.encode(query)}"
   resp = Net::HTTP.get_response(URI.parse(url))
   data = resp.body
 
+  #it conforms it with ruby's arrays
   result = JSON.parse(data)
-	
+
   return result
 end
 
@@ -23,6 +25,7 @@ end
 puts "Searching for #{query}..." 
 
 results = hash_search( query )
+
 if (results["found"] == "false")
   $stderr.puts("\033[31mNo matching records found for #{query}.\033[0m")
   exit
@@ -30,11 +33,11 @@ end
 
 puts "Input type: #{results["type"]}."
 
-
-
 if (results["type"] != "cleartext")
+  #if the argument is an hash just output the clear text
   puts "\033[32mClear text: \033[0m" + results["hashes"]["cleartext"]
 else
+  #or if it is clear text let's oputput all the matching hashes!
   results["hashes"].each do |entry|
     if (entry[0] != "cleartext")
       puts "\033[32m" + entry[0].upcase + "\033[0m: " + entry[1]
